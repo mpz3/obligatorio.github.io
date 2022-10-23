e01ea27411d13a08bf0bb3d097da0f20eae88e29
CAMILA MEDINA <cmedina@tiendamia.com>
2022-10-23 20:58:15 -0300

    codigo actualizado

---
+++ js/clasesDeNegocios.js
@@ -0,0 +1,40 @@
+class UsuarioImportador {
+    constructor() {
+        this.nombre = "";
+        this.foto = "";
+        this.user = "";
+        this.contraseña = "";
+    }
+}
+class empresa {
+    static idEmpresa = 0;
+    constructor() {
+        this.nombre = "";
+        this.foto = "";
+        this.user = "";
+        this.contraseña = "";
+    }
+}
+
+class SolicitudCarga {
+    static idSolicitudCarga = 0;
+    constructor() {
+        this.id = -1;
+        this.estado = "";
+        this.descripcion = "";
+        this.tipo = "";
+        this.puerto = "";
+        this.cantidadContenedores = -1;
+        this.idEmpresa = -1;
+        this.userImportador = "";
+    }
+}
+
+class Buques {
+    constructor() {
+        this.nombreBuque = "";
+        this.cargaMaxima = "";
+        this.fechaLlegada = "";
+        //aca luego tendremos que asociar el nombre de la empresa dueña del buque
+    }
+}
--- js/codigo.js
+++ js/codigo.js
@@ -1,5 +1,5 @@
 function nuevoRegistro(nombre, foto, usuario, pass) {
-    let user = new Usuario();
+    let user = new UsuarioImportador();
     user.nombre = nombre;
     user.foto = foto;
     user.user = usuario;
@@ -42,29 +42,64 @@ function validarContrasena(passw) {
     }
     return false;
 }
+function quitarFakePath(pNombreArchivo) {
+    let nombre = "";
+    let posBarra = -1;
+    let posBuscando = pNombreArchivo.length - 1;
+    while (posBuscando >= 0 && posBarra === -1) {
+        let letraX = pNombreArchivo.charAt(posBuscando);
+        if (letraX === "\\" || letraX === "/") {
+            posBarra = posBuscando;
+        }
+        posBuscando--;
+    }
+    for (let i = posBarra + 1; i < pNombreArchivo.length; i++) {
+        nombre += pNombreArchivo.charAt(i);
+    }
+    return nombre;
+}
 function validarDatosMercaderia(pdesc, ptipo, ppuerto, pcantContenedores, pidEmpresa) {
     if (pdesc === "" || ptipo === "" || ppuerto === "" || pcantContenedores === "" || isNaN(pcantContenedores) || pidEmpresa === "" || isNaN(pidEmpresa)) {
+        if (pdesc === "") document.querySelector("#txtDescrip").style.borderColor = "red";
+        else document.querySelector("#txtDescrip").style.borderColor = "black";
+
+        if (ptipo === "") document.querySelector("#txtTipoCarga").style.borderColor = "red";
+        else document.querySelector("#txtTipoCarga").style.borderColor = "black";
+
+        if (ppuerto === "") document.querySelector("#txtPuerto").style.borderColor = "red";
+        else document.querySelector("#txtPuerto").style.borderColor = "black";
+
+        if (pcantContenedores === "" || isNaN(pcantContenedores)) document.querySelector("#txtCantContenedores").style.borderColor = "red";
+        else document.querySelector("#txtCantContenedores").style.borderColor = "black";
+
+        if (pidEmpresa === "" || isNaN(pidEmpresa)) document.querySelector("#txtIdEmpresa").style.borderColor = "red";
+        else document.querySelector("#txtIdEmpresa").style.borderColor = "black";
+
         return false;
     }
     return true;
 }
 function ingresarMercaderia(pdesc, ptipo, ppuerto, pcantContenedores, pidEmpresa) {
     let nuevaSolicitud = new SolicitudCarga();
-    nuevaSolicitud.id = 4; //todo incrementar id
+    nuevaSolicitud.id = SolicitudCarga.idSolicitudCarga;
     nuevaSolicitud.estado = "Pendiente";
     nuevaSolicitud.descripcion = pdesc;
     nuevaSolicitud.tipo = ptipo;
     nuevaSolicitud.puerto = ppuerto;
     nuevaSolicitud.cantidadContenedores = pcantContenedores;
     nuevaSolicitud.idEmpresa = pidEmpresa;
+    nuevaSolicitud.userImportador = userOnline;
     solicitudesDeCarga.push(nuevaSolicitud);
+    SolicitudCarga.idSolicitudCarga++;
+    return SolicitudCarga.idSolicitudCarga - 1;
 }
 
-function busquedaSolicitudes(busqueda) {
-    let tabla = `<table border="1"><tr><th>ID</th><th>Descripcion</th></tr>`;
+
+function busquedaSolicitudesPendientes(busqueda) {
+    let tabla = `<table border="1" style="text-align: center;"><tr><th>ID</th><th>Estado</th><th>Descripcion</th><th>Tipo</th><th>Puerto Origen</th><th>Nro de contenedores</th><th>ID Empresa</th></tr>`;
     for (let i = 0; i < solicitudesDeCarga.length; i++) {
         if (solicitudesDeCarga[i].id === Number(busqueda) || solicitudesDeCarga[i].descripcion.toLowerCase() === busqueda.toLowerCase()) {
-            tabla += `<tr><td>${solicitudesDeCarga[i].id}</td><td>${solicitudesDeCarga[i].descripcion}</td></tr>`;
+            tabla += `<tr><td>${solicitudesDeCarga[i].id}</td><td>${solicitudesDeCarga[i].estado}</td><td>${solicitudesDeCarga[i].descripcion}</td><td>${solicitudesDeCarga[i].tipo}</td><td>${solicitudesDeCarga[i].puerto}</td><td>${solicitudesDeCarga[i].cantidadContenedores}</td><td>${solicitudesDeCarga[i].idEmpresa}</td></tr>`;
         }
     }
     tabla += `</table>`;
